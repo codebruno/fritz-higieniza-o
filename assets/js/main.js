1b43c859661a617eeacaec66ca183b0a488e2c77
codebruno <b.webdevs@gmail.com>
2024-10-20 11:21:47 -0300

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Seleciona os links de navegação dentro da navbar
-    const navLinks = document.querySelectorAll('.nav-link');
-    const navbarCollapse = document.querySelector('.navbar-collapse');
-  
-    // Adiciona o evento de clique em cada link
-    navLinks.forEach(link => {
-      link.addEventListener('click', function () {
-        // Verifica se o menu está aberto e fecha
-        if (navbarCollapse.classList.contains('show')) {
-          new bootstrap.Collapse(navbarCollapse).toggle();
-        }
-      });
+  // Seleciona os links de navegação dentro da navbar
+  const navLinks = document.querySelectorAll('.nav-link');
+  const navbarCollapse = document.querySelector('.navbar-collapse');
+
+  // Adiciona o evento de clique em cada link
+  navLinks.forEach(link => {
+    link.addEventListener('click', function () {
+      // Verifica se o menu está aberto e fecha
+      if (navbarCollapse.classList.contains('show')) {
+        new bootstrap.Collapse(navbarCollapse).toggle();
+      }
     });
   });
+});
